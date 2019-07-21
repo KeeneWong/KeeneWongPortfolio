eabcf74d3d787501a45b9aaeded45fb50d5759ff
KeeneWong <keenewonghofung@gmail.com>
2019-07-21 01:30:37 -0400

    some scroll function and project expand

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 const navitem = document.querySelectorAll('.navitem');
+const getprojectsdiv = document.querySelectorAll('.projectsdiv');
 
+
+
+
+
+//Nav Bar Button and auto scroll to section
 navitem.forEach(item=>{
     item.addEventListener('click',function(){
         document.querySelector(`.${item.getAttribute('data-scroll')}`).scrollIntoView({behavior: "smooth"});
     })
 })
+
+
+//Display the project detail
+getprojectsdiv.forEach(function(div, i){
+    div.addEventListener('click',function(){
+        document.querySelectorAll('.projectdivExpand').forEach(div=>div.classList.add('hidden'));
+        document.querySelector('.scrollprojectdivExpand').classList.add('hidden');
+        document.querySelector('.scrollprojectdivExpand').classList.toggle('hidden');
+        document.querySelector(`.${div.getAttribute('data-expand')}`).classList.toggle('hidden');
+        document.querySelector('.scrollprojectdivExpand').scrollIntoView({behavior: "smooth"});
+    })
+})
+
+//BacktoProjectDiv button
+document.querySelectorAll('.backtoprojectdiv').forEach(function(backbtn, i){
+    backbtn.addEventListener('click',function(){
+        document.querySelector('.scrollprojectmain').scrollIntoView({behavior: "smooth"});
+        setTimeout(function(){
+        document.querySelectorAll('.projectdivExpand')[i].classList.add('hidden')
+        document.querySelector('.scrollprojectdivExpand').classList.toggle('hidden');},500);
+        
+    })
+
+
+})
+
+
+
 
 
 
